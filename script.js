fdb6aa9f7c7e485b84bee7455e7fbca6a9999620
Monté <crazybudderpie@gmail.com>
2025-07-01 22:15:45 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.getElementById("input")
+const list = document.getElementById("List")
+
+function addTask(){
+    if (input.value === ''){
+        alert("You to enter a task")
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = input.value;
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        li.appendChild(span)
+        list.appendChild(li);
+    }
+    input.value = '';
+    saveData();
+}
+
+list.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+
+    else if(e.target.tagName === "SPAN"){
+                e.target.parentElement.remove();
+                saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML)
+}
+
+function showTaskList(){
+    list.innerHTML = localStorage.getItem("data");
+}
+showTaskList();
